a65ef2e6024c65ff62af6bae8b14636a0f425541
Mark Striemer <mstriemer@mozilla.com>
2014-01-26 16:47:56 -0600

    store track listing

--- app/scripts/tracks.js
+++ app/scripts/tracks.js
@@ -3,6 +3,7 @@ define(['localForage'], function (localForage) {
 
     function Track() {
         this.id = 'track-' + new Date().getTime();
+        this.addedToTracks = false;
         this.saveTimeout = null;
         this.lastSaveTime = null;
         this.points = [];
@@ -20,10 +21,21 @@ define(['localForage'], function (localForage) {
         },
         save: function () {
             console.log('saving track ' + this.id);
-            localForage.setItem(this.id, this.points).then(function () {
+            localForage.setItem(this.id, this.points).then(function (tracks) {
                 this.lastSaveTime = new Date();
                 this.saveTimeout = null;
-            });
+                console.log('updated last save');
+            }.bind(this));
+            if (!this.addedToTracks) {
+                this.addedToTracks = true;
+                // This seems like a genuinely bad idea...
+                localForage.getItem('tracks').then(function (tracks) {
+                    tracks = tracks || [];
+                    tracks.push(this.id);
+                    localForage.setItem('tracks', tracks);
+                    console.log('updated tracks');
+                }.bind(this));
+            }
         },
     };
 
